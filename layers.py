eb181fd110490b98720ae86c4fb1aa564b1354e4
HarishBachu <bachuharish577@gmail.com>
2021-10-03 19:26:04 +0530

    Added activation response

--- layers.py
+++ layers.py
@@ -1,18 +1,17 @@
 import numpy as np 
 
-
 class Dense:
-    def __init__(self, units, input_shape = None):
+    def __init__(self, units, input_shape = None, activation = None):
         self.units = units
         
-        assert input_shape[1] == 1
-        assert len(input_shape) == 2 
-
         self.input_shape = input_shape 
         self.output_shape = (units, 1)
 
-        self.W = np.random.rand(input_shape[0], units)      #Layer Weight Initializing
-        self.B = np.random.rand(units, 1)          #Layer Bias Initializing
+        self.activation = activation 
+
+    def init_params(self):
+        self.W = np.random.rand(self.input_shape[0], self.units)      #Layer Weight Initializing
+        self.B = np.random.rand(self.units, 1)          #Layer Bias Initializing
 
     def getShapes(self): 
         print("W:", self.W.shape)
@@ -22,4 +21,7 @@ class Dense:
         assert input_tensor.shape[1] == 1
         assert len(input_tensor.shape) == 2 
 
-        return self.W.T.dot(input_tensor) + self.B
+        out = self.W.T.dot(input_tensor) + self.B 
+
+        return out if self.activation == None else self.activation(out)
+            
